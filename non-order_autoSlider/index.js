48e07cc72a438660f224b4c1451d8c60c0b211d7
ahmetcan-7 <80100428+ahmetcan-7@users.noreply.github.com>
2021-03-20 23:50:41 +0300

    Add files via upload

---
+++ non-order_autoSlider/index.js
@@ -0,0 +1,47 @@
+var images = [
+
+    {
+        name: "image_1",
+        image: "/image/image1.jpg",
+        link: "https://en.wikipedia.org/wiki/England"
+    },
+    {
+        name: "image_2",
+        image: "/image/image2.jpg",
+        link: "https://www.britannica.com/place/England"
+    },
+    {
+        name: "image_3",
+        image: "/image/image3.jpg",
+        link: "https://www.visitengland.com/"
+    },
+    {
+        name: "image_4",
+        image: "/image/image4.jpg",
+        link: "https://www.gov.uk/guidance/national-lockdown-stay-at-home"
+    }
+
+];
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let rand = Math.floor(Math.random() * images.length);
+let x = 5;
+setInterval(() => {
+
+    while (x === rand) {
+        rand = Math.floor(Math.random() * images.length);
+    }
+
+    const getimg = document.querySelector("#image");
+    const getlink = document.querySelector("#link");
+    const getname = document.querySelector("#header");
+
+    getimg.setAttribute("src", images[rand].image);
+    getlink.setAttribute("href", images[rand].link);
+    getname.innerHTML = images[rand].name;
+
+
+
+    x = rand;
+}, 1000);
+
